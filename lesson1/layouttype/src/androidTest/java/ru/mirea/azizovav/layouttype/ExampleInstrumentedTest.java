7a57b6f36685d7b4ff297883e6ba0e16b67ac29b
Amir Azizov <creator005@mail.ru>
2024-08-26 18:45:48 +0300

    fix

--- lesson1/layouttype/src/androidTest/java/ru/mirea/azizovav/layouttype/ExampleInstrumentedTest.java
+++ lesson1/layouttype/src/androidTest/java/ru/mirea/azizovav/layouttype/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("ru.mirea.dyachenkoas.layouttype", appContext.getPackageName());
+        assertEquals("ru.mirea.azizovav.layouttype", appContext.getPackageName());
     }
 }

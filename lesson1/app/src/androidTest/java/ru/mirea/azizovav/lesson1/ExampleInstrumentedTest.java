326e9e466db737d8ca8f7dc612911693455c4f00
Amir Azizov <creator005@mail.ru>
2024-08-26 17:49:49 +0300

    changes

--- lesson1/app/src/androidTest/java/ru/mirea/azizovav/lesson1/ExampleInstrumentedTest.java
+++ lesson1/app/src/androidTest/java/ru/mirea/azizovav/lesson1/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("ru.mirea.dyachenkoas.lesson1", appContext.getPackageName());
+        assertEquals("ru.mirea.azizovav.lesson1", appContext.getPackageName());
     }
 }
--- lesson1/buttonclicker/src/androidTest/java/ru/mirea/azizovav/buttonclicker/ExampleInstrumentedTest.java
+++ lesson1/buttonclicker/src/androidTest/java/ru/mirea/azizovav/buttonclicker/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("ru.mirea.dyachenkoas.buttonclicker", appContext.getPackageName());
+        assertEquals("ru.mirea.azizovav.buttonclicker", appContext.getPackageName());
     }
 }
